a621a99bb5a05b0e28fe0541c8b8bf325833cf53
GitHub Action <41898282+github-actions[bot]@users.noreply.github.com>
2022-01-13 20:38:59 -0500

    :star: GenStage example

---
+++ scrapper/lib/scrapper.ex
@@ -0,0 +1,15 @@
+defmodule Scrapper do
+  @moduledoc """
+  Documentation for `Scrapper`.
+  """
+
+  def work() do
+    # For simplicity, this function is
+    # just a placeholder and does not contain
+    # real scraping logic.
+    1..5
+    |> Enum.random()
+    |> :timer.seconds()
+    |> Process.sleep()
+  end
+end
