d1d4896cc147b292bb505efd1bd2d116e81f260a
Rajesh Rajagopalan <rrajesh1979@gmail.com>
2022-01-17 21:20:24 -0500

    :star: Clean-up done

--- ref_elixir/lib/payment_proc_utils.ex
+++ ref_elixir/lib/payment_proc_utils.ex
@@ -16,23 +16,4 @@ defmodule PaymentProc.Utils do
 
     payment_streams
   end
-
-  @spec parse_row(String) :: Stream
-  def parse_row(row) do
-    [row] = CSV.parse_string(row, skip_headers: false)
-
-    %{
-      id: Enum.at(row, 0),
-      tenant: Enum.at(row, 1),
-      type: Enum.at(row, 2),
-      value: Enum.at(row, 3),
-      created_at: Enum.at(row, 4),
-      status: Enum.at(row, 5)
-    }
-  end
-
-  @spec filter_data(Map) :: boolean()
-  def filter_data(doc) do
-    doc.status == "CLOSED" || doc.status == "status"
-  end
 end
--- ref_elixir/lib/skip_invalid_records.ex
+++
@@ -1,6 +0,0 @@
-defmodule SkipInvalidRecords do
-  use Opus.Pipeline
-  require Logger
-
-  skip(:skip_record, if: &(&1.status === "CLOSED"))
-end
